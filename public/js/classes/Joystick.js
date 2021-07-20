4a2e3b305070d6f2cf4ea4bf524f98fbed153e2d
Nathan Pham <nathanpham.me@gmail.com>
2021-07-20 01:37:54 -0700

    finished joystick class

--- public/js/classes/Joystick.js
+++ public/js/classes/Joystick.js
@@ -1,12 +1,35 @@
-class Joystick {
-    constructor({container=document.body}={}) {
+export default class Joystick {
+    constructor({container=document.body, onMove}={}) {
         this.container = container
+        this.onMove = onMove || (() => {})
+
+        this.radius = 40
         this.thumb = this.createThumb()
+
+		this.origin = {left: this.thumb.offsetLeft, top: this.thumb.offsetTop}
+        this.offset = {x: 0, y: 0}
+
+        this.touch
+            ? this.thumb.addEventListener("touchstart", this.mouseDown.bind(this))
+            : this.thumb.addEventListener("mousedown", this.mouseDown.bind(this))
+    }
+
+    get touch() {
+        return "ontouchstart" in window || navigator.msMaxTouchPoints
+    }
+
+    mouseDown(e) {
+        e.stopPropagation()
+        e.preventDefault()
+        this.tap(e)
+        return false
     }
 
     createThumb() {
         const shell = document.createElement("div")
         shell.style.cssText = `
+            backdrop-filter: blur(2px);
+            user-select: none;
             position: fixed;
             bottom: 1.5rem;
             left: 50%;
@@ -23,8 +46,8 @@ class Joystick {
         `
         const thumb = document.createElement("div")
         thumb.style.cssText = `
-            height: 2rem;
-            width: 2rem;
+            height: 2.5rem;
+            width: 2.5rem;
             background: #000;
             border-radius: 50%;
         `
@@ -34,96 +57,66 @@ class Joystick {
         return thumb
     }
 
-    getMousePosition(e) {}
+    mouse(e){
+        return {
+            x: e.targetTouches ? e.targetTouches[0].pageX : e.clientX,
+            y: e.targetTouches ? e.targetTouches[0].pageY : e.clientY
+        }
+	}
 
+	move(e){
+        const mouse = this.mouse(e)
+       
+        let left = mouse.x - this.offset.x
+        let top = mouse.y - this.offset.y
+
+        const mag = Math.pow(left, 2) + Math.pow(top, 2)
+        if(mag > Math.pow(this.radius, 2)) {
+            const dist = Math.sqrt(mag)
+            left = (left / dist) * this.radius
+            top = (top / dist) * this.radius
+        }
+
+        this.thumb.style.transform = `translate(${left}px, ${top}px)`
+        const forward = -(top - this.origin.top + this.thumb.clientHeight/2)/this.radius;
+		const turn = (left - this.origin.left + this.thumb.clientWidth/2)/this.radius;
+		this.onMove([forward, turn])
+	}
+
+    tap(e) {
+		this.offset = this.mouse(e)
+
+        const mouseMove = e => {
+            e.preventDefault()
+            this.move(e)
+            return false
+        }
+
+        const mouseUp = e => {
+            e.preventDefault()
+            this.up(e)
+            return false
+        }
+
+		if (this.touch) {
+			document.ontouchmove = mouseMove
+			document.ontouchend =  mouseUp
+		} else{
+			document.onmousemove = mouseMove
+			document.onmouseup = mouseUp
+		}
+    }
+
+    up() {
+        if (this.touch) {
+            document.ontouchmove = null
+            document.touchend = null
+        } else {
+            document.onmousemove = null
+            document.onmouseup = null
+        }
+        
+        this.thumb.style.transform = ""
+        this.onMove([0, 0])
+    }    
 }
-new Joystick()
-// class JoyStick{
-// 	constructor(options){
-// 		const thumb = document.createElement("div");
-// 		thumb.style.cssText = "position: absolute; left: 20px; top: 20px; width: 40px; height: 40px; border-radius: 50%; background: #fff;";
-// 		circle.appendChild(thumb);
-// 		document.body.appendChild(circle);
-// 		this.domElement = thumb;
-// 		this.maxRadius = options.maxRadius || 40;
-// 		this.maxRadiusSquared = this.maxRadius * this.maxRadius;
-// 		this.onMove = options.onMove;
-// 		this.game = options.game;
-// 		this.origin = { left:this.domElement.offsetLeft, top:this.domElement.offsetTop };
-// 		this.rotationDamping = options.rotationDamping || 0.06;
-// 		this.moveDamping = options.moveDamping || 0.01;
-// 		if (this.domElement!=undefined){
-// 			const joystick = this;
-// 			if ('ontouchstart' in window){
-// 				this.domElement.addEventListener('touchstart', function(evt){ evt.preventDefault(); joystick.tap(evt); evt.stopPropagation();});
-// 			}else{
-// 				this.domElement.addEventListener('mousedown', function(evt){ evt.preventDefault(); joystick.tap(evt); evt.stopPropagation();});
-// 			}
-// 		}
-// 	}
-	
-// 	getMousePosition(evt){
-// 		let clientX = evt.targetTouches ? evt.targetTouches[0].pageX : evt.clientX;
-// 		let clientY = evt.targetTouches ? evt.targetTouches[0].pageY : evt.clientY;
-// 		return { x:clientX, y:clientY };
-// 	}
-	
-// 	tap(evt){
-// 		evt = evt || window.event;
-// 		// get the mouse cursor position at startup:
-// 		this.offset = this.getMousePosition(evt);
-// 		const joystick = this;
-// 		if ('ontouchstart' in window){
-// 			document.ontouchmove = function(evt){ evt.preventDefault(); joystick.move(evt); };
-// 			document.ontouchend =  function(evt){ evt.preventDefault(); joystick.up(evt); };
-// 		}else{
-// 			document.onmousemove = function(evt){ evt.preventDefault(); joystick.move(evt); };
-// 			document.onmouseup = function(evt){ evt.preventDefault(); joystick.up(evt); };
-// 		}
-// 	}
-	
-// 	move(evt){
-// 		evt = evt || window.event;
-// 		const mouse = this.getMousePosition(evt);
-// 		// calculate the new cursor position:
-// 		let left = mouse.x - this.offset.x;
-// 		let top = mouse.y - this.offset.y;
-// 		//this.offset = mouse;
-		
-// 		const sqMag = left*left + top*top;
-// 		if (sqMag>this.maxRadiusSquared){
-// 			//Only use sqrt if essential
-// 			const magnitude = Math.sqrt(sqMag);
-// 			left /= magnitude;
-// 			top /= magnitude;
-// 			left *= this.maxRadius;
-// 			top *= this.maxRadius;
-// 		}
-// 		// set the element's new position:
-// 		this.domElement.style.top = `${top + this.domElement.clientHeight/2}px`;
-// 		this.domElement.style.left = `${left + this.domElement.clientWidth/2}px`;
-		
-// 		const forward = -(top - this.origin.top + this.domElement.clientHeight/2)/this.maxRadius;
-// 		const turn = (left - this.origin.left + this.domElement.clientWidth/2)/this.maxRadius;
-		
-// 		if (this.onMove!=undefined) this.onMove.call(this.game, forward, turn);
-// 	}
-	
-// 	up(evt){
-// 		if ('ontouchstart' in window){
-// 			document.ontouchmove = null;
-// 			document.touchend = null;
-// 		}else{
-// 			document.onmousemove = null;
-// 			document.onmouseup = null;
-// 		}
-// 		this.domElement.style.top = `${this.origin.top}px`;
-// 		this.domElement.style.left = `${this.origin.left}px`;
-		
-// 		this.onMove.call(this.game, 0, 0);
-// 	}
-// }
-
-// new JoyStick({
-//     onMove: (...args) => console.log(args)
-// })
