cd577117244b0ec212d192d49c531b0bc34e93be
Nathan Pham <nathanpham.me@gmail.com>
2021-07-20 23:15:26 -0700

    hide bounding boxes

--- public/js/classes/objects/player/PlayerBounds.js
+++ public/js/classes/objects/player/PlayerBounds.js
@@ -9,9 +9,9 @@ export default class PlayerBounds {
 
     createBounds() {
         this.geometry = new THREE.BoxGeometry(100, 300, 100)
-        this.material = new THREE.MeshBasicMaterial({color: 0x222222, wireframe: true})
+        this.material = new THREE.MeshBasicMaterial({color: 0x222222, opacity: 0, transparent: true})
 
-        //  opacity: 0, transparent: true
+        //  
         const bounds = new THREE.Mesh(this.geometry, this.material)
         bounds.position.y = (this.geometry.parameters.height / 2) + 100
         return bounds
@@ -25,8 +25,11 @@ export default class PlayerBounds {
             shape: new CANNON.Box(new CANNON.Vec3(width / 2, height / 2, depth / 2)),
         })
         cube.position.copy(this.object.position)
-        cube.angularDamping = 0.75
+        cube.angularDamping = 0.99
+        cube.linearDamping = 0.99
 
+        // cube.velocity.mult(0)
+        cube.angularVelocity.mult(0)
         return cube
     }
 
@@ -35,14 +38,11 @@ export default class PlayerBounds {
         const speed = player.currentAnimation == "running" ? 400 : 150
         const z = speed * delta * (forward > 0 ? 1 : forward < 0 ? -1/3 : 0)
 
-        this.cannon.angularVelocity.set(0, -turn * delta * 200, 0)
-
+        // this.cannon.quaternion.z += -turn * delta
+        this.cannon.angularVelocity.set(0, -turn * delta * 400, 0)
         const relative = new CANNON.Vec3(0, 0, z)
         this.cannon.quaternion.vmult(relative, relative)
         this.cannon.position.vadd(relative, this.cannon.position)
-
-
-//         let relativeVector = new CANNON.Vec3(0,0,100);
 
 // // Use quaternion to rotate the relative vector, store result in same vector
 // body.quaternion.vmult(relativeVector, relativeVector);
